d44db095e31089057ac8454a7a6d2165c4b8c21b
NataliKontakt <natalikontaktgood@gmail.com>
2025-04-11 15:43:26 +0500

    Add annotation

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,6 +1,8 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
@@ -8,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
+@Controller
 public class PostController {
     private static final String APPLICATION_JSON = "application/json";
     private static final String CONTENT_TYPE_HEADER = "Content-Type";
