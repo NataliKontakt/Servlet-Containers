2d0a897da585eae3de586392313bb162ce7a05a7
natali-kontakt <natalikontaktgood@gmail.com>
2025-04-13 22:11:51 +0500

    Migration

---
+++ src/main/java/ru/netology/AppConfig.java
@@ -0,0 +1,11 @@
+package ru.netology;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("ru.netology")
+@EnableWebMvc
+public class AppConfig {
+}
--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,49 +1,37 @@
 package ru.netology.controller;
 
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
-    private static final String APPLICATION_JSON = "application/json";
-    private static final String CONTENT_TYPE_HEADER = "Content-Type";
-
     private final PostService service;
-    private final Gson gson;
 
     public PostController(PostService service) {
         this.service = service;
-        this.gson = new Gson();
     }
 
-    public void all(HttpServletResponse response) throws IOException {
-        response.setHeader(CONTENT_TYPE_HEADER, APPLICATION_JSON);
-        final var data = service.all();
-        response.getWriter().print(gson.toJson(data));
+    @GetMapping
+    public List<Post> all() {
+        return service.all();
     }
 
-    public void getById(long id, HttpServletResponse response) throws IOException {
-        response.setHeader(CONTENT_TYPE_HEADER, APPLICATION_JSON);
-        final var data = service.getById(id);
-        response.getWriter().print(gson.toJson(data));
+    @GetMapping("/{id}")
+    public Post getById(@PathVariable long id) {
+        return service.getById(id);
     }
 
-    public void save(Reader body, HttpServletResponse response) throws IOException {
-        response.setHeader(CONTENT_TYPE_HEADER, APPLICATION_JSON);
-        final var post = gson.fromJson(body, Post.class);
-        final var data = service.save(post);
-        response.getWriter().print(gson.toJson(data));
+    @PostMapping
+    public Post save(@RequestBody Post post) {
+        return service.save(post);
     }
 
-    public void removeById(long id, HttpServletResponse response) {
+    @DeleteMapping("/{id}")
+    public void removeById(@PathVariable long id) {
         service.removeById(id);
-        response.setStatus(HttpServletResponse.SC_NO_CONTENT);
     }
 }
